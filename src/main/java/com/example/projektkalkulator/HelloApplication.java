7970621ff6f231d255d8bae6f92f9c76ec90caf8
Maksymilian74 <273007@student.pwr.edu.pl>
2023-06-03 23:52:58 +0200

    Drobne poprawki i praca nad wyglądem okienka

--- src/main/java/com/example/projektkalkulator/HelloApplication.java
+++ src/main/java/com/example/projektkalkulator/HelloApplication.java
@@ -3,7 +3,10 @@ package com.example.projektkalkulator;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,8 +14,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 460, 600);
         HelloController hello = fxmlLoader.getController();
+        stage.setResizable(false);
+        scene.setFill(Color.TRANSPARENT);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setTitle("Kalkulator");
         stage.setScene(scene);
         stage.show();
